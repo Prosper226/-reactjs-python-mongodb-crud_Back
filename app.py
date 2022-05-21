b6043e8f72ef4c31d78bde80413b8074b2c58c47
Prosper226 <psedgo@gmail.com>
2022-05-21 13:40:19 +0000

    first commit

--- app.py
+++ app.py
@@ -10,4 +10,64 @@ from flask_cors import CORS
 import yaml
 
 
+app     =   Flask(__name__)
+config  =   yaml.load(open('database.yaml'))
+client  =   MongoClient(config['uri'])
+db      =   client['knowledge_factory']
+USERS   =   db['users']
 
+CORS(app)
+
+@app.route('/')
+def index():
+    return render_template('home.html')
+
+
+@app.route('/users', methods=['POST, GET'])
+def data():
+
+    # POST a data to database
+    if request.method == 'POST':
+        body    =   request.json
+        print(body)
+        firstName   =   body['firstName']
+        lastName    =   body['lastName']
+        emailId     =   body['emailId']
+
+        USERS.insert_one({
+            "firstName"   :   firstName,
+            "lastName"    :   lastName,
+            "emailId"     :   emailId
+        })
+
+        return jsonify({
+            'status'    :   'Data is posted to MongoDB!',
+            'firstName' :   firstName,
+            'lastName'  :   lastName,
+            'emailId'   :   emailId
+        })
+
+    # if request.method == 'GET':
+    #     allData     =   USERS.find()
+    #     dataJson    =   []
+    #     for data in allData:
+    #         id          =   data['_id']
+    #         firstName   =   data['firstName']
+    #         lastName    =   data['lastName']
+    #         emailId     =   data['emailId']
+
+    #         dataDict    = {
+    #             'id'          :   str(id)
+    #             'firstName'   :   firstName,
+    #             'lastName'    :   lastName,
+    #             'emailId'     :   emailId
+    #         }
+    #         dataJson.append(dataDict)
+    #     print(dataJson)
+    #     return dataJson
+
+
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
